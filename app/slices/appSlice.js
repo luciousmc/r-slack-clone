3c939d1c3650f49d280f3d8002d6eb5d27ddcf35
Marlon Clay <luciousmc@icloud.com>
2021-09-16 14:15:03 -0700

    redux-toolkit, material icons and styled components added

---
+++ app/slices/appSlice.js
@@ -0,0 +1,7 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const appSlice = createSlice({
+  name: 'app',
+  initialState: {},
+  reducers: {},
+});
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>myapp</h1>
+    <div className='App'>
+      <Router>
+        <>
+          <Switch>
+            <Route path='/' exact>
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
